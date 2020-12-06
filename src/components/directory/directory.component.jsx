878689dbe05d2a8e4f81608284754ec82da4a5fe
@shahasad78 <shahasad78@gmail.com>
2020-12-06 00:19:59 -0800

    update our menu items to navigate using withRouter HOC.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {MenuItem} from "../menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
+
+
 
 import './directory.styles.scss'
 
@@ -13,33 +15,33 @@ class Directory extends Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: ''
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: ''
                 }
             ]
         }
@@ -49,7 +51,8 @@ class Directory extends Component {
        return  <div className="directory-menu">
             {
                 this.state.sections.map(
-                ({title, imageUrl, id, size}) => <MenuItem key={id} title={title} subtitle="SHOP NOW" imageUrl={imageUrl} size={size}/>
+                ({id, ...otherSectionProps}) =>
+                    <MenuItem key={id} {...otherSectionProps}/>
             )}
         </div>
     }
