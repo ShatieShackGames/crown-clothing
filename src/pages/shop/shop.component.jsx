858535c00439ceae3602567dd8c88fcce7b7a4eb
@shahasad78 <shahasad78@gmail.com>
2021-01-10 23:42:41 -0800

    - created new selectors for isLoading and isCollectionsLoaded - created actions and reducers for our isLoading and isCollectionsLoaded data - refactored shop.component to get its loading status and shopping data from redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,38 +1,56 @@
 import React from 'react';
 import {Route} from "react-router";
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
 
+import {fetchCollectionsStartAsync} from "../../redux/shop/shop.actions";
+import {selectIsCollectionFetching, selectIsCollectionLoaded} from "../../redux/shop/shop.selectors";
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../category/collection.component";
 
-import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
-import {updateCollections} from "../../redux/shop/shop.actions";
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const { updateCollections } = this.props;
-
-        const collectionRef = firestore.collection('collections');
-        collectionRef.onSnapshot( async snapshot => {
-            updateCollections(convertCollectionsSnapshotToMap(snapshot));
-        })
+        const {fetchCollectionsStartAsync} = this.props;
+        fetchCollectionsStartAsync();
     }
 
     render() {
-        const { match } = this.props;
-      return (
-          <div>
-              <Route exact path={`${match.path}`} component={CollectionsOverview} />
-              <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-          </div>
-      )
-  }
+        const {match, isCollectionFetching, isCollectionsLoaded } = this.props;
+        return (
+            <div>
+                <Route exact
+                       path={`${match.path}`}
+                       render={(props) => (
+                           <CollectionsOverviewWithSpinner
+                                isLoading={isCollectionFetching} {...props}
+                           />
+                       )}
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    render={(props) => (
+                        <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}
+                        />
+                    )}
+                />
+            </div>
+        )
+    }
 }
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionLoaded
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from "redux";
 import { persistStore } from "redux-persist";
-
 import logger from "redux-logger";
+import thunk from 'redux-thunk';
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const middlewares = [thunk];
 
 // We only want to log in the development build of our app
 if (process.env.NODE_ENV === 'development') {
@@ -15,5 +15,3 @@ if (process.env.NODE_ENV === 'development') {
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
-export default { store, persistor };
